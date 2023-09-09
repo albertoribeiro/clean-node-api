97d5b6e400e85733f45921c614694354be652081
albertoribeiro <alberto.r.flavio@gmail.com>
2023-09-09 13:43:12 -0400

    refactor: move mongodb methods to a helper

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   mongodbMemoryServerOptions: {
-    binary: {
-      version: '4.0.3',
-      skipMD5: true
-    },
     instance: {
       dbName: 'jest'
+    },
+    binary: {
+      version: '4.0.1', // Version of MongoDB
+      skipMD5: true
     },
     autoStart: false
   }
---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,18 @@
+import { Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as unknown as MongoClient,
+
+  async connect (url: string): Promise<void> {
+    console.log('URL MONGO')
+    console.log(url)
+    this.client = await MongoClient.connect(url)
+  },
+  async disconnect (): Promise<void> {
+    await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  }
+}
