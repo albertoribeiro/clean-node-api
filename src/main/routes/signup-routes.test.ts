3c811bbeb76d4eac601dc22eb3545573469ed86b
albertoribeiro <alberto.r.flavio@gmail.com>
2023-09-14 21:26:44 -0400

    refactor: connect to mongoDB before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,10 +1,25 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('Signup Routes', () => {
+  beforeAll(async () => {
+    const mongoURL: string = process.env.MONGO_URL ?? ''
+    await MongoHelper.connect(mongoURL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should retur an account on success', async () => {
     await request(app)
-      .post('/api/singup')
+      .post('/api/signup')
       .send({
         name: 'Alberto',
         email: 'alberto.r.flavio@gmail.com',
