2369af999d33afa6360bba04b2e04febf53e856a
albertoribeiro <alberto.r.flavio@gmail.com>
2023-09-14 22:08:11 -0400

    feat:connect to mongodb before app listen

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -1,10 +1,13 @@
 module.exports = {
+  mongodbMemoryServer: {
+    version: 'latest'
+  },
   mongodbMemoryServerOptions: {
     instance: {
       dbName: 'jest'
     },
     binary: {
-      version: '4.0.1', // Version of MongoDB
+      version: '4.2.0', // Version of MongoDB
       skipMD5: true
     },
     autoStart: false
